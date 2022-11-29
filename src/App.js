161b8adcd250f2473dff25ea418a9e28a5f000f6
hussain2023 <iamhussainali2@gmail.com>
2022-11-29 16:04:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import { color } from "@mui/system";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Button color="secondary">Material</Button>
+      </div>
+      <Button
+        variant="contained"
+        onClick={() => {
+          alert("clicked");
+          color = "secondary";
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 }
